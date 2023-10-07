0c4f4904252fbecce95d42f817ca6a20bde46175
Bhargav Chiramdasu <2100030053@kluniversity.in>
2023-10-08 00:00:54 +0530

    081020231200AM

---
+++ EmailNotification-1/src/main/java/com/klu/email/EmailNotification1Application.java
@@ -0,0 +1,13 @@
+package com.klu.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailNotification1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailNotification1Application.class, args);
+	}
+
+}
